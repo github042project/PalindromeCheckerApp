8d9c0fee1b3a97152f4b87764d32c631eb9fc5a9
github042project <gv6320@srmist.edu.in>
2026-02-24 15:02:35 +0530

    garvita added uc3

--- App/src/UseCase2PalindromeCheckerApp.java
+++ App/src/UseCase2PalindromeCheckerApp.java
@@ -1,24 +1,18 @@
-package PACKAGE_NAME;
-
 public class UseCase2PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Hardcoded string
-        String word = "madam";
-
-        // Reverse the string
+        String original = "level";
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        // Check if palindrome
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome.");
         } else {
-            System.out.println(word + " is NOT a Palindrome.");
+            System.out.println(original + " is NOT a Palindrome.");
         }
     }
 }
